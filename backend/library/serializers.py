57665bc7fee68ba2c3b272e0f40e0e1a0a2ed9a8
Patryk Bochenek <pb339@protonmail.com>
2024-07-23 00:48:49 +0100

    Add expiration_date to BookSerializer

--- backend/library/serializers.py
+++ backend/library/serializers.py
@@ -8,11 +8,11 @@ class ReaderSerializer(serializers.ModelSerializer):
 
 class BookSerializer(serializers.ModelSerializer):
     readers = ReaderSerializer(many=True, read_only=True)
+    expiration_date = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', required=False)
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'is_available', 'readers']
-
+        fields = ['id', 'title', 'author', 'is_available', 'readers', 'checked_out_at', 'expiration_date']
 class ReaderDetailSerializer(serializers.ModelSerializer):
     books_taken = BookSerializer(many=True, read_only=True)
 
