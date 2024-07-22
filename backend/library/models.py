768972602eb9582eff7e1333421c96019e1e7a09
Patryk Bochenek <pb339@protonmail.com>
2024-07-22 21:26:25 +0100

    Create Book model

--- backend/library/models.py
+++ backend/library/models.py
@@ -5,3 +5,11 @@ class Reader(models.Model):
 
     def __str__(self):
         return self.name
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    available = models.BooleanField(default=True)
+    reader = models.ForeignKey(Reader, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
