75eeab50842993521d08e9fac98aba64efa59d42
Patryk Bochenek <pb339@protonmail.com>
2024-07-22 23:15:14 +0100

    Add Navigation to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <nav class="bg-gray-800 p-4">
+      <div class="container mx-auto flex justify-between items-center">
+        <a class="text-white text-xl font-bold" href="#">Library App</a>
+        <div>
+          <router-link class="text-white mx-2" to="/">Books</router-link>
+          <router-link class="text-white mx-2" to="/readers">Readers</router-link>
+        </div>
+      </div>
+    </nav>
+    <div class="container mx-auto mt-6">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
