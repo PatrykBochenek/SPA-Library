3be7fbb8056ebd0c7dee52c62aa17ab8a56ea89c
Patryk Bochenek <pb339@protonmail.com>
2024-07-22 23:37:39 +0100

    Update ManyToMany Book & Reader relationship

--- backend/library/models.py
+++ backend/library/models.py
@@ -1,15 +1,21 @@
+# myapp/models.py
+
 from django.db import models
 
-class Reader(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    available = models.BooleanField(default=True)
-    reader = models.ForeignKey(Reader, on_delete=models.SET_NULL, null=True, blank=True)
+    author = models.CharField(max_length=100)
+    is_available = models.BooleanField(default=True) 
+    readers = models.ManyToManyField('Reader', related_name='books_taken', blank=True) 
 
     def __str__(self):
         return self.title
+
+class Reader(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def has_taken_out(self, book):
+        return book in self.books_taken.all()
