1fde32a865cb1b8072271a0e5308e03f44aaba85
Patryk Bochenek <pb339@protonmail.com>
2024-07-23 00:48:26 +0100

    Add Expiration Date functionality

--- backend/library/models.py
+++ backend/library/models.py
@@ -1,15 +1,31 @@
-# myapp/models.py
-
 from django.db import models
+from datetime import timedelta
+from django.utils import timezone
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    is_available = models.BooleanField(default=True) 
-    readers = models.ManyToManyField('Reader', related_name='books_taken', blank=True) 
+    is_available = models.BooleanField(default=True)
+    readers = models.ManyToManyField('Reader', related_name='books_taken', blank=True)
+    checked_out_at = models.DateTimeField(null=True, blank=True)
+    expiration_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    def check_out(self, reader):
+        self.is_available = False
+        self.checked_out_at = timezone.now()
+        self.expiration_date = self.checked_out_at + timedelta(days=30)  
+        self.save()
+        self.readers.add(reader)
+
+    def return_book(self):
+        self.is_available = True
+        self.checked_out_at = None
+        self.expiration_date = None
+        self.save()
+        self.readers.clear()
 
 class Reader(models.Model):
     name = models.CharField(max_length=200)
