34d5565df6dcc85fa091c8834caf6bf1961c0930
Patryk Bochenek <pb339@protonmail.com>
2024-07-22 23:01:12 +0100

    Update CORS_ALLOWED_ORIGINS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,7 +53,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
 
 ROOT_URLCONF = 'backend.urls'
 
