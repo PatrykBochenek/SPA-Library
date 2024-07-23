870c96a0181115cfa7f3206a9248b9f4da12d7c6
Patryk Bochenek <pb339@protonmail.com>
2024-07-23 03:23:27 +0100

    Add readers and requested_by to BookSerializer

--- backend/library/serializers.py
+++ backend/library/serializers.py
@@ -1,18 +1,25 @@
 from rest_framework import serializers
 from .models import Book, Reader
 
-class ReaderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Reader
-        fields = ['id', 'name']
-
 class BookSerializer(serializers.ModelSerializer):
-    readers = ReaderSerializer(many=True, read_only=True)
+    readers = serializers.SerializerMethodField()
     expiration_date = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', required=False)
+    requested_by = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'is_available', 'readers', 'checked_out_at', 'expiration_date']
+        fields = ['id', 'title', 'author', 'is_available', 'readers', 'checked_out_at', 'expiration_date', 'requested_by']
+
+    def get_readers(self, obj):
+        return [{'id': reader.id, 'name': reader.name} for reader in obj.readers.all()]
+
+class ReaderSerializer(serializers.ModelSerializer):
+    books_taken = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Reader
+        fields = ['id', 'name', 'books_taken']
+
 class ReaderDetailSerializer(serializers.ModelSerializer):
     books_taken = BookSerializer(many=True, read_only=True)
 
