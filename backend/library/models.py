89cbfe2c1a36d84a3be868f5b89cc20ca0c4668c
Patryk Bochenek <pb339@protonmail.com>
2024-07-23 02:23:28 +0100

    Add requested_by to Book model

--- backend/library/models.py
+++ backend/library/models.py
@@ -9,6 +9,7 @@ class Book(models.Model):
     readers = models.ManyToManyField('Reader', related_name='books_taken', blank=True)
     checked_out_at = models.DateTimeField(null=True, blank=True)
     expiration_date = models.DateTimeField(null=True, blank=True)
+    requested_by = models.ForeignKey('Reader', null=True, blank=True, on_delete=models.SET_NULL, related_name='requested_books')
 
     def __str__(self):
         return self.title
@@ -16,7 +17,8 @@ class Book(models.Model):
     def check_out(self, reader):
         self.is_available = False
         self.checked_out_at = timezone.now()
-        self.expiration_date = self.checked_out_at + timedelta(days=30)  
+        self.expiration_date = self.checked_out_at + timedelta(days=30)
+        self.requested_by = None
         self.save()
         self.readers.add(reader)
 
