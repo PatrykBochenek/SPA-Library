b6ace79a3f036c07b4ada173386af92c1325628a
Patryk Bochenek <pb339@protonmail.com>
2024-07-23 02:22:40 +0100

    Make Logo redirect back to BookList

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav class="bg-gray-800 p-4">
       <div class="container mx-auto flex justify-between items-center">
-        <a class="text-white text-xl font-bold" href="#">Library App</a>
+        <a class="text-white text-xl font-bold" href="/">Library App</a>
         <div>
           <router-link class="text-white mx-2" to="/">Books</router-link>
           <router-link class="text-white mx-2" to="/readers">Readers</router-link>
